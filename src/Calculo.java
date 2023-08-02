06cd68c6edb8c1dfccff407ac6eaaa775ec87e8b
Rafael da Silva Muniz <rafaelmunizrsm@hotmail.com>
2023-08-02 16:54:52 -0300

    Exercicio Módulo 10

--- src/Calculo.java
+++ src/Calculo.java
@@ -5,16 +5,50 @@
 
 public class Calculo {
 
-    private float md = 0.0f;
+    private float md;
 
-    private float n1 = 10.0f;
-    private float n2 = 30.0f;
-    private float n3 = 50.0f;
-    private float n4 = 80.0f;
+    private float n1;
+    private float n2;
+    private float n3;
+    private float n4;
+
+    public float getN1() {
+        return n1;
+    }
+
+    public void setN1(float n1) {
+        this.n1 = n1;
+    }
+
+    public float getN2() {
+        return n2;
+    }
+
+    public void setN2(float n2) {
+        this.n2 = n2;
+    }
+
+    public float getN3() {
+        return n3;
+    }
+
+    public void setN3(float n3) {
+        this.n3 = n3;
+    }
+
+    public float getN4() {
+        return n4;
+    }
+
+    public void setN4(float n4) {
+        this.n4 = n4;
+    }
 
     public float retornaMedia() {
 
+
         md = (n1 + n2 + n3 + n4)/4;
+       
         return md;
 
     }
--- src/Media.java
+++ src/Media.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
 public class Media {
 
     public static void main(String args[]) {
-         Calculo cal = new Calculo();
 
-         float f = new Float(cal.retornaMedia());
+        Calculo cal = new Calculo();
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Média: " + f);
-    }
+
+        System.out.println("Digite a primeira nota: ");
+        float num1 = sc.nextFloat();
+        cal.setN1(num1);
+
+        System.out.println("Digite a segunda nota: ");
+        float num2 = sc.nextFloat();
+        cal.setN2(num2);
+
+        System.out.println("Digite a terceira nota: ");
+        float num3 = sc.nextFloat();
+        cal.setN3(num3);
+
+        System.out.println("Digite a quarta nota: ");
+        float num4 = sc.nextFloat();
+        cal.setN4(num4);
+
+
+        float f = new Float(cal.retornaMedia());
+        
+
+        if (f >= 7.0) {
+            
+        	System.out.println("Aprovado : " + f);
+        
+            
+        }else if ( f >= 5.0 && f < 7.0 ) {
+           
+        	System.out.println("Recuperacao :  " + f);
+        
+        }else if (f < 5.0) {
+        	System.out.println("Reprovado :  " + f);
+        }
+        
+    }  
 }
+        
